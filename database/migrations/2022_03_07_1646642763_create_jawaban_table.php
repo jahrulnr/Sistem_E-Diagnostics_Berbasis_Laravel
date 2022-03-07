a49384d46971529999ff4276e96f0166be92f72f
Jahrul Novario <73051140+jahrulnr@users.noreply.github.com>
2022-03-07 17:23:43 +0700

    Update 2022_03_07_1646642763_create_jawaban_table.php

--- database/migrations/2022_03_07_1646642763_create_jawaban_table.php
+++ database/migrations/2022_03_07_1646642763_create_jawaban_table.php
@@ -9,7 +9,7 @@ class CreateJawabanTable extends Migration
     public function up()
     {
         Schema::create('jawaban', function (Blueprint $table) {
-    		$table->integer('id_jawaban',100);
+    		$table->integer('id_jawaban')->length(100)->unsigned();
     		$table->string('npm',9);
     		$table->integer('id_soal',100);
     		$table->text('jawaban_mhs');
